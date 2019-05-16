91fe71ab58bc10e8515a9298cf7fb8b082cea6f1
Josivan Santos <josivan.santos@outlook.com>
2019-05-16 19:56:16 -0400

    inictial

---
+++ IntegerDivision/IntegerDivision/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace IntegerDivision
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtFirstInteger = new System.Windows.Forms.TextBox();
+            this.txtSecondInteger = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the first integer value:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter second integer value:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(106, 167);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(251, 167);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // txtFirstInteger
+            // 
+            this.txtFirstInteger.Location = new System.Drawing.Point(251, 75);
+            this.txtFirstInteger.Name = "txtFirstInteger";
+            this.txtFirstInteger.Size = new System.Drawing.Size(108, 20);
+            this.txtFirstInteger.TabIndex = 5;
+            this.txtFirstInteger.TextChanged += new System.EventHandler(this.txtFirstInteger_TextChanged);
+            // 
+            // txtSecondInteger
+            // 
+            this.txtSecondInteger.Location = new System.Drawing.Point(251, 116);
+            this.txtSecondInteger.Name = "txtSecondInteger";
+            this.txtSecondInteger.Size = new System.Drawing.Size(120, 20);
+            this.txtSecondInteger.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(105, 216);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(266, 43);
+            this.txtResult.TabIndex = 7;
+            this.txtResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecondInteger);
+            this.Controls.Add(this.txtFirstInteger);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "IntegerDivision";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtFirstInteger;
+        private System.Windows.Forms.TextBox txtSecondInteger;
+        private System.Windows.Forms.RichTextBox txtResult;
+    }
+}
+
---
+++ IntegerDivision/IntegerDivision/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntegerDivision
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            decimal firstInteger;
+            decimal secondInteger;
+            decimal answer;
+
+            flag = decimal.TryParse(txtFirstInteger.Text, out firstInteger);
+            if (flag == false)
+            {
+                MessageBox.Show("The first value it´s not a number: " + txtFirstInteger.Text);
+                return;
+                }
+
+            flag = decimal.TryParse(txtSecondInteger.Text, out secondInteger);
+            if (flag == false)
+
+            {
+                MessageBox.Show("Enter the whole number", "imput error");
+                txtSecondInteger.Focus();
+                return;
+            }
+            answer = firstInteger / secondInteger;
+
+            txtResult.Text = firstInteger.ToString() + " divided by " +
+            secondInteger.ToString() + " equals " + answer.ToString();
+
+            txtResult.Visible = true;
+        }
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtFirstInteger_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    } }
